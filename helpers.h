b3574a9c47cb873612e55f0263a6d5deb901c467
enatheme <enatheme@enatheme.org>
2020-01-12 14:49:20 +0800

    function latency comparator

--- helpers.h
+++ helpers.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <algorithm>
 #include <chrono>
+#include <future>
+#include <vector>
 
 namespace helpers {
 
@@ -10,7 +13,33 @@ auto latency_counter(F func, Args ...args)
     auto start = std::chrono::high_resolution_clock::now();
     func(args...);
     auto end = std::chrono::high_resolution_clock::now();
-    return 5;//std::chrono::duration_cast(Granularity);
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+}
+
+template <typename F, typename ...Args>
+auto latency_counter_average(F func, int iteration, Args ...args)
+{
+    double count = 0;
+    for(int i = 0 ; i < iteration ; ++i)
+    {
+        count += latency_counter(func, args...);
+    }
+    return count / iteration;
+}
+
+// returns the fastest function, does not validate the result
+template <typename F, typename ...Args>
+auto fastest_function(int iteration, std::vector<F> funcs, Args ...args)
+{
+    std::vector<std::tuple<double /* latency */, F>> functions_latency;
+    std::for_each(funcs.begin(), funcs.end(), [&](auto f) {
+        double latency = latency_counter_average(f, iteration, args...);
+        functions_latency.push_back({latency, f});
+    });
+
+    return std::get<1>(*std::min_element(functions_latency.begin(), functions_latency.end(), [](auto & f1, auto & f2) {
+        return std::get<0>(f1) < std::get<0>(f2);
+    }));
 }
 
 } // namespace helpers
--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include "helpers.h"
 
+#include <cassert>
 #include <iostream>
 #include <thread>
 
@@ -8,7 +9,23 @@ void f(int a, int b)
     std::this_thread::sleep_for(std::chrono::seconds(a + b));
 }
 
+void f2(int a, int b)
+{
+    std::cout << "must be displayed" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+}
+
+void f3(int a, int b)
+{
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+}
+
 int main()
 {
-    std::cout << helpers::latency_counter<>(&f, 1, 2) << std::endl;
+    std::cout << helpers::latency_counter_average<>(&f, 2, 1, 2) << std::endl;
+
+    std::vector<decltype(&f2)> fs = {f, f2, f3};
+    auto fu = helpers::fastest_function(1, fs, 2, 3);
+    fu(2, 3);
+    assert(fu == &f2);
 }
