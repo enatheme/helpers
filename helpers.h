44c115bbeb849450d1f0bdd4df60ec3119a8252c
enatheme <enatheme@enatheme.org>
2020-01-13 23:28:24 +0800

    Add async latency calculator

--- helpers.h
+++ helpers.h
@@ -2,7 +2,9 @@
 
 #include <algorithm>
 #include <chrono>
+#include <cmath>
 #include <future>
+#include <numeric>
 #include <vector>
 
 namespace helpers {
@@ -39,6 +41,60 @@ auto fastest_function(int iteration, std::vector<F> funcs, Args ...args)
 
     return std::get<1>(*std::min_element(functions_latency.begin(), functions_latency.end(), [](auto & f1, auto & f2) {
         return std::get<0>(f1) < std::get<0>(f2);
+    }));
+}
+
+template <typename F, typename ...Args>
+double latency_counter_average_parallel(F func, int iteration, Args ...args)
+{
+    std::vector<std::future<long int>> latencies(iteration);
+    std::for_each(latencies.begin(), latencies.end(), [&] (auto & l) {
+        l = std::async([func, args...] {
+            return latency_counter(func, args...);
+        });
+    });
+    double count = 0;
+    std::for_each(latencies.begin(), latencies.end(), [&count] (auto & l) {
+        l.wait();
+        count += l.get();
+    });
+    return count / iteration;
+    // @TODO figure out why accumulate fails here
+    return (std::accumulate(latencies.begin(), latencies.end(), 0, [] (auto accumulator, auto & rhs) {
+        rhs.wait();
+        return accumulator + rhs.get();
+    }) / iteration);
+}
+
+// returns the fastest function, does not validate the result
+template <typename F, typename ...Args>
+auto fastest_function_parallel(int iteration, std::vector<F> funcs, Args ...args)
+{
+    float epsilon = 0.01f;
+    std::vector<std::tuple<std::future<double>, double /* latency retrieved */, F>> functions_latency;
+    std::for_each(funcs.begin(), funcs.end(), [&](auto & f) {
+        functions_latency.push_back({std::async([&f, &iteration, &args...] {
+            return latency_counter_average_parallel(f, iteration, args...);
+        }), 0, f});
+    });
+
+    for (auto & latency : functions_latency) {
+        std::get<0>(latency).wait();
+        auto val = std::get<0>(latency).get();
+        std::get<1>(latency) = val;
+    }
+    auto ret = std::get<2>(functions_latency[0]);
+    auto lll = std::get<1>(functions_latency[0]);
+    std::for_each(functions_latency.begin(), functions_latency.end(), [&ret, &lll](auto & latency) {
+        if (std::get<1>(latency) < lll) {
+            ret = std::get<2>(latency);
+            lll = std::get<1>(latency);
+        }
+    });
+    return ret;
+    // @TODO figure out why min_element fails here
+    return std::get<2>(*std::min_element(functions_latency.begin(), functions_latency.end(), [&epsilon](auto & f1, auto & f2) {
+        return std::abs(std::get<1>(f1) - std::get<1>(f2)) < epsilon;
     }));
 }
 
--- test.cpp
+++ test.cpp
@@ -6,18 +6,20 @@
 
 void f(int a, int b)
 {
+    std::cout << "f(" << a << ", " << b << ")" << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(a + b));
 }
 
 void f2(int a, int b)
 {
-    std::cout << "must be displayed" << std::endl;
+    std::cout << "f2(" << a << ", " << b << ")" << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 void f3(int a, int b)
 {
-    std::this_thread::sleep_for(std::chrono::seconds(3));
+    std::cout << "f3(" << a << ", " << b << ")" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
 }
 
 int main()
@@ -25,7 +27,9 @@ int main()
     std::cout << helpers::latency_counter_average<>(&f, 2, 1, 2) << std::endl;
 
     std::vector<decltype(&f2)> fs = {f, f2, f3};
-    auto fu = helpers::fastest_function(1, fs, 2, 3);
+    auto fu = helpers::fastest_function_parallel(4, fs, 2, 3);
     fu(2, 3);
-    assert(fu == &f2);
+    assert(fu != f3);
+    assert(fu != f);
+    assert(fu == f2);
 }
